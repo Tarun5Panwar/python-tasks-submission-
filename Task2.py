4558278752e7ba8edd606dc026c19c9a14224fa1
Tarun5Panwar <tarun7665@gmail.com>
2025-07-04 21:30:23 +0530

    Add files via upload

--- Task2.py
+++ Task2.py
@@ -1,11 +1,3 @@
-# Initialize variables
-num = 1
-total = 0
-
-# Loop from 1 to 50
-while num <= 50:
-    total = total + num
-    num = num + 1
-
-# Print the result
-print("Sum of integers from 1 to 50 is:", total)
+a = int(input("Enter the number"))
+if a>5:
+    print("The number is greater than 5")
---
+++ task1.py
@@ -0,0 +1,5 @@
+num = int(input("Enter a number"))
+if num % 2 == 0:
+    print(num, "This is an even number")
+else:
+    print(num, "This is an odd number")
