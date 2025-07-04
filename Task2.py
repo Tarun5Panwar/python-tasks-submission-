78b8db7b69dc2263af2ad28fe64f6256aacc867f
Tarun5Panwar <tarun7665@gmail.com>
2025-07-04 21:17:22 +0530

    Add files via upload

--- Task2.py
+++ Task2.py
@@ -1,3 +1,11 @@
-a =(input("Enter your First Name"))
-b =(input("Enter your Last Name"))
-print("Hello " + a + "" + b + " Welcome to python program")
+# Initialize variables
+num = 1
+total = 0
+
+# Loop from 1 to 50
+while num <= 50:
+    total = total + num
+    num = num + 1
+
+# Print the result
+print("Sum of integers from 1 to 50 is:", total)
